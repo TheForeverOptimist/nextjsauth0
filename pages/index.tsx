561e9c9cfdeb0a22e8a17890329d4be426473335
TheForeverOptimist <sean@elevate.app>
2023-06-29 14:28:35 -0400

    updated login

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import {useUser} from "@auth0/nextjs-auth0/client";
 
 function index() {
-  return (
-    <div>index</div>
-  )
+    const {user, error, isLoading } = useUser();
+
+    return <a href="/api/auth/login">Login</a>
 }
 
 export default index
